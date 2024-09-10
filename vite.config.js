6a8cc9a2b1af55b93932d3cd12d5fbd0ca678257
clarapenn <49952437+clarapenn@users.noreply.github.com>
2024-09-10 16:28:09 +0100

    Tweaks for building site in vite
    Now all the files end up in the dist/ directory, not just the index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import {defineConfig} from "vite"
 
 export default defineConfig({
-	plugins: [
-		
+	build: {
+		outDir: 'dist', // Change this to your desired output directory
+		assetsDir: '', // Set this to an empty string or your desired assets directory
+		rollupOptions: {
+		input: {
+			main: './index.html', // Specify the main HTML file
+			project1: './project-1.html',
+			project2: './project-2.html',
+			project3: './project-3.html',
+		  },
+		},
+	  },
+	  plugins: [
+
 	]
 })
